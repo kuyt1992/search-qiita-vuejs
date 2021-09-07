192f984546774bdb3d803c64bc7e51c055691166
kuyt1992 <kuyt1992@gmail.com>
2021-09-07 12:16:46 +0900

    feat: Qiita APIを叩くまでを実装

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,36 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+      items: null,
+      keyword: '',
+      message: ''
+    },
+    watch: {
+      
+    },
+    created: function() {
+      this.keyword = 'JavaScript'
+      this.getAnswer()
+    },
+    methods: {
+      getAnswer: function() {
+        if(this.keyword === '') {
+          this.items = null
+          return
+        }
+        this.message = 'Loading...'
+        var vm = this
+        var params = { page: 1, per_page: 20, query: this.keyword }
+        axios.get('https://qiita.com/api/v2/items', { params })
+        .then(function(res){
+          vm.items = res.data
+        })
+        .catch(function(err) {
+          vm.message = 'Error' + err
+        })
+        .finally(function() {
+          vm.message = ''
+        })
+      }
+    }
 })
